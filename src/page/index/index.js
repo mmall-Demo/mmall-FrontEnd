4e40efb450adcf0ddfe69128f43d1454008a0276
lzb <1255863672@qq.com>
2020-12-29 16:20:31 +0800

    商品模块的设计与功能开发 1.首页左侧关键字导航界面开发 2.首页Banner及F楼层界面开发 3.商品列表页开发 4.商品详情页的开发

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,16 +2,29 @@
  * @Author: luzhibin
  * @Date: 2020-12-07 15:30:08
  * @Last Modified by: luzhibin
- * @Last Modified time: 2020-12-07 16:01:23
+ * @Last Modified time: 2020-12-22 14:03:16
  */
 
-"use strict";
-require("page/common/nav/index.js");
-require("page/common/header/index.js");
-require("page/common/nav-side/index.js");
-var navSide = require("page/common/nav-side/index.js");
-var _mm = require("util/mm.js");
+'use strict';
+require('./index.css');
+require('page/common/nav/index.js');
+require('page/common/header/index.js');
+require('util/slider/index.js');
+var navSide         = require('page/common/nav-side/index.js');
+var templateBanner  = require('./banner.string');
+var _mm             = require('util/mm.js');
 
-navSide.init({
-  name: "user-center",
+$(function() {
+    // 渲染banner的html
+    var bannerHtml  = _mm.renderHtml(templateBanner);
+    $('.banner-con').html(bannerHtml);
+    // 初始化banner
+    var $slider     = $('.banner').unslider({
+        dots: true
+    });
+    // 前一张和后一张操作的事件绑定
+    $('.banner-con .banner-arrow').click(function(){
+        var forward = $(this).hasClass('prev') ? 'prev' : 'next';
+        $slider.data('unslider')[forward]();
+    });
 });
---
+++ src/util/slider/index.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: luzhibin 
+ * @Date: 2020-12-22 13:56:06 
+ * @Last Modified by:   luzhibin 
+ * @Last Modified time: 2020-12-22 13:56:06 
+ */
+'use strict';
+
+require('./index.css');
+require('./unslider.js');
